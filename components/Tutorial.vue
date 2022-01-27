1c15e261bac0786a8d9efb67ee2b42350c9ceeee
Gilmar Santos <g_afrosantos@icloud.com>
2022-01-27 16:11:33 -0300

    Configuração inicial do projeto

---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <h1>Configuração inicial</h1>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
+
+<style lang="scss"></style>
