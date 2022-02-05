3c243997e4002cb76b008b4de9b9b272892ca804
Gilmar Santos <g_afrosantos@icloud.com>
2022-02-05 20:22:52 -0300

    Adicionando teste de login com github

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,11 +1,11 @@
 import NextAuth from "next-auth";
-import GithubProvide from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   providers: [
-    GithubProvide({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 });
