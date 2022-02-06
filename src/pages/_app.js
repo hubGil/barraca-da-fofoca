f227ac2a18c2a391ae0339826f426ef8efc35b91
Gilmar Santos <g_afrosantos@icloud.com>
2022-02-05 21:12:56 -0300

    Adicionando secret para opcoes do nextauth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import { Footer } from "../components/organisms/Footer";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  console.log(session);
   return (
     <SessionProvider session={session}>
       <Header />
