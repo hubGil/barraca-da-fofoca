508abfda3e0041d7f8539ef53bd4fe8d300fa865
Janderson Santos <janderson.santos@valtech.com>
2022-02-16 16:47:34 -0300

    fixing small code error

--- src/components/atoms/BtnHamburg/index.js
+++ src/components/atoms/BtnHamburg/index.js
@@ -1,13 +1,12 @@
 import styles from "./styles.module.scss";
-export function BtnHamburg({ open, setOpen, ...props }) {
+export function BtnHamburg({ open, opened, ...props }) {
   const isExpanded = open ? true : false;
 
   return (
     <button
       aria-label="Toggle menu"
       aria-expanded={isExpanded}
-      open={open}
-      onClick={() => setOpen(!open)}
+      onClick={() => opened(!open)}
       {...props}
       className={`${styles.buttonContainer} ${
         open ? "" : styles.buttonContainer_open
