54781866a4fec4ffb4d934508b83449088e87365
Janderson Santos <janderson.santos@valtech.com>
2022-02-18 15:21:42 -0300

    Fix head crossOrigin attr

--- src/components/molecules/Head/Head.js
+++ src/components/molecules/Head/Head.js
@@ -11,7 +11,7 @@ const SiteHead = ({ children }) => {
 
       {/* Fonts */}
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link
         href="https://fonts.googleapis.com/css2?family=Merienda+One&family=Playfair+Display:wght@400;700;800;900&display=swap"
         rel="stylesheet"
