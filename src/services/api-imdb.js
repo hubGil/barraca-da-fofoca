76ac6893e3cc9d0992bc54b3db4d0bb5a55c4646
elainemattje <55418000+elainemattje@users.noreply.github.com>
2022-02-22 15:19:13 -0300

    correção conflitos

--- src/services/api-imdb.js
+++ src/services/api-imdb.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import axios from "axios";
-
-/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
-
-const apiImdb = axios.create({
-  baseURL: "https://imdb8.p.rapidapi.com",
-  withCredentials: false,
-  headers: {
-    "x-rapidapi-host": "imdb8.p.rapidapi.com",
-    "x-rapidapi-key": "1e9b7d195cmshb669202f8842d16p1a42a2jsncaa10ba319cf",
-  },
-});
-
-export default {
-  getAutoComplete(id) {
-    return apiImdb.get("/auto-complete", { params: { q: id } });
-  },
-
-  getAllNews(id) {
-    return apiImdb.get("/actors/get-all-news", { params: { nconst: id } });
-  },
-  getFamousInfo(id) {
-    return apiImdb.get("/actors/get-bio", { params: { nconst: id } });
-  },
-};
-=======
 import axios from "axios";
 
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
@@ -50,4 +23,3 @@ export default {
     return apiImdb.get("/actors/get-bio", { params: { nconst: id } });
   },
 };
->>>>>>> fe/jand/split-head
