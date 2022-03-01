7b7009a80aff1ab769a8a013d88df3039a1c86aa
Gilmar Santos <g_afrosantos@icloud.com>
2022-03-01 20:21:29 -0300

    atualizando head do app

--- src/components/molecules/Head/Head.js
+++ src/components/molecules/Head/Head.js
@@ -11,13 +11,17 @@ const SiteHead = ({ Title }) => {
 
       {/* Fonts */}
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
       <link
         href="https://fonts.googleapis.com/css2?family=Merienda+One&family=Playfair+Display:wght@400;700;800;900&display=swap"
         rel="stylesheet"
       />
     </Head>
   );
-}
+};
 
 export default SiteHead;
